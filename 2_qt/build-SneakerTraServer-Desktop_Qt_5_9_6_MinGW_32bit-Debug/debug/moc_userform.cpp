f2e81dde97702106cbf76d225c7d1edc7ea9e789
Ivan <293450544@qq.com>
2020-04-07 22:03:09 +0800

    second master

--- 2_qt/build-SneakerTraServer-Desktop_Qt_5_9_6_MinGW_32bit-Debug/debug/moc_userform.cpp
+++ 2_qt/build-SneakerTraServer-Desktop_Qt_5_9_6_MinGW_32bit-Debug/debug/moc_userform.cpp
@@ -6,7 +6,7 @@
 ** WARNING! All changes made in this file will be lost!
 *****************************************************************************/
 
-#include "../../SpeakerTraServer/subwidgets/userform.h"
+#include "../../SneakerTraServer/subwidgets/userform.h"
 #include <QtCore/qbytearray.h>
 #include <QtCore/qmetatype.h>
 #if !defined(Q_MOC_OUTPUT_REVISION)
